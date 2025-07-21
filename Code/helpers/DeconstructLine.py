bf1749dfcc90f2adfc818a23a3bd2e311b0406cd
Sam Gou <samir.gouhary@gmail.com>
2025-07-21 09:58:05 +0100

    delted unnecessary code files

--- Code/helpers/DeconstructLine.py
+++
@@ -1,9 +0,0 @@
-def deconstruct_line(line):
-    block_loc_map = {"G":[],"O":[],"#":[]}
-    for idx,block in enumerate(line):
-        block_loc_map[block].append(idx)
-    return block_loc_map
-
-if __name__ == "__main__":
-    line = "G#O#O"
-    print(deconstruct_line(line))
